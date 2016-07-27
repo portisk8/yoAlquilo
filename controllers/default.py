beaf94dc9deb22a75bc976f306d30f4cac0350b5
morghan marco santiago acuña <anita.pokorasky@gmail.com>
2016-07-27 16:13:16 -0300

    home creada. falta hacer el mapa responsive y hacer funcionar etoggle

--- controllers/default.py
+++ controllers/default.py
@@ -10,15 +10,19 @@
 
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
-
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    
+    from gluon.tools import geocode
+    latitude = longtitude = ''
+    form=SQLFORM.factory(Field('search'), _class='form-search')
+    form.custom.widget.search['_class'] = 'input-long search-query'
+    form.custom.submit['_value'] = 'Search'
+    form.custom.submit['_class'] = 'btn'
+    if form.accepts(request):
+        address=form.vars.search
+        (latitude, longitude) = geocode(address)
+    else:
+        (latitude, longitude) = ('','')
+    return dict(form=form, latitude=latitude, longitude=longitude)
 
 
 def user():

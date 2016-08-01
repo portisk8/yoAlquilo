0c09c3c7d7be292c3236ed8ea2ef1be121769df9
Augusto Portillo <Augusto Portillo>
2016-07-31 21:47:54 -0300

    modifique el mapa en casaController y vista casaController/index, ahora muestra la ubicacion de la casa seleccionada

--- controllers/casaController.py
+++ controllers/casaController.py
@@ -1,5 +1,10 @@
+from gluon.serializers import loads_json
+
+global idCasa
+
 def index():
     idNum= getId(URL(args=request.args, vars=request.get_vars, host=True))
+    idCasa = idNum
     casa= db.casa(idNum)
     marker= db.marker(casa.id_marker)
     contacto = db.usuario(marker.id_user)
@@ -14,6 +19,41 @@ def getId(url):
         cont=cont+1
     numId=url[cn+1: len(url)]
     return numId
+
+def getId2(url):
+    longitud=len(url)
+    c=longitud
+    cont=0
+    for i in range(longitud):
+        c=c-1
+        if (url[c]!='/'):
+            cont+=1
+        else:
+            break
+    ini=longitud - cont
+    numId = url[ini:longitud]
+    return numId
+
+
+def getMarker():
+    idNum= getId(URL(args=request.args, vars=request.get_vars, host=True))
+    idNum = getId2(idNum)
+    casa= db.casa(idNum)
+    marker= db.marker(casa.id_marker)
+    if(casa.disponible=="Disponible"):
+        icono = '../static/icons/icon_green32.png'
+        disp ="<p><font color='green'>"+casa.disponible+"</font></p>"
+    else:
+        icono = '../static/icons/icon_pink32.png'
+        disp ="<p><font color='red'>"+casa.disponible+"</font></p>"
+    mapa = {
+        'lat': marker.lat,
+        'lng': marker.lng,
+        'title': casa.nombre,
+        'infoWindow': { 'content': '<h4>' + casa.nombre +"</h4>"+disp },
+        'icon':icono,
+        }
+    return response.json(mapa)
 
 def download():
     return response.download(request, db)
